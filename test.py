77c1548353ff94101a1213e0b54c7952d9918273
sanchayana2007 <sanchayana2007@gmail.com>
2021-01-21 00:28:31 +0530

    Update and rename test.py to main.py

--- test.py
+++
@@ -1,9 +0,0 @@
-import pytest
-# test_capitalize.py
-def capital_case(x):
-    return x.capitalize()
-
-def test_capital_case():
-    assert capital_case('semaphore') == 'Semaphore'
-    
-test_capital_case()
